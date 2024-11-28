602ddcd66b06161da3a9070f45907c3d5e906220
UnperishedFoods <131722267+UnperishedFoods@users.noreply.github.com>
2024-11-28 12:24:46 -0500

    Add files via upload

---
+++ server_scripts/forbiddenArcanus.js
@@ -0,0 +1,30 @@
+ServerEvents.recipes((event) => {
+	event.shaped(
+	"8x forbidden_arcanus:darkstone",
+		[
+			"BBB",
+			"BAB",
+			"BBB",
+		],
+		{
+			A: "forbidden_arcanus:dark_matter",
+			B: "minecraft:stone"
+		},
+	)
+	event.remove({ id: 'forbidden_arcanus:mundabitur_dust' })
+		event.recipes.create.crushing('forbidden_arcanus:mundabitur_dust', 'forbidden_arcanus:rune')
+	event.recipes.ars_nouveau.imbuement(
+		'ars_nouveau:source_gem',
+		'forbidden_arcanus:rune',
+		500,
+		['minecraft:pink_dye', '#quark:corundum', 'minecraft:redstone'],
+	)
+	event.recipes.ars_nouveau.imbuement(
+		'minecraft:glass',
+		'forbidden_arcanus:arcane_crystal',
+		1000,
+		[],
+	)
+})
+
+console.log('Forbidden & Arcanus loaded')
