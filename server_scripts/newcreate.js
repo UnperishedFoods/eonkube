b6ba71bde3ae4ae376bf80cd0a4f9cc96a862dd0
BoxPatata <Moclements2009@gmail.com>
2024-11-28 19:43:44 -0500

    lapis automation

--- server_scripts/newcreate.js
+++ server_scripts/newcreate.js
@@ -1,6 +1,21 @@
 ServerEvents.recipes((event) => {
-    event.shapeless('minecraft:diorite', [
-    'minecraft:cobblestone',
-    'minecraft:quartz'
-    ])
+    event.shapeless(
+        "minecraft:diorite", 
+        [
+            "minecraft:cobblestone",
+            "minecraft:quartz",
+        ],
+    )
+    event.splashing(
+        [
+            "minecraft:lapis_lazuli",
+        ],
+        "createbb:white_phosphorus", 
+    )
+    event.milling(
+        [
+            "createbb:white_phosphorus", Item.of("createbb:white_phosphorus").withChance(0.75)
+        ],
+        "create:limestone"
+    )
 })
