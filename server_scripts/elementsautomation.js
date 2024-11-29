dcc540b83449f13fa5b96eb732b0678dac1797c8
BoxPatata <Moclements2009@gmail.com>
2024-11-29 13:28:39 -0500

    commas galore

--- server_scripts/elementsautomation.js
+++ server_scripts/elementsautomation.js
@@ -3,6 +3,7 @@ ServerEvents.recipes((event) => {
         [
             Item.of("mekanism:dirty_dust_uranium").withChance(0.06),
             Item.of("kubejs:thorium_grit").withChance(0.06),
-        ]
+        ],
+        "create_things_and_misc:crushed_magma",
     )
 })
