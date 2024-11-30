39648cd4d4633f7e2cac75f99eef1de8a5842130
BoxPatata <Moclements2009@gmail.com>
2024-11-29 19:07:41 -0500

    WHAT THE ACTUAL F**K IS YOUR FORMATTING

--- server_scripts/elementsautomationserver.js
+++ server_scripts/elementsautomationserver.js
@@ -1,37 +1,45 @@
 ServerEvents.recipes((event) => {
-    event.recipes.create.splashing(
-        [
-            Item.of("mekanism:dirty_dust_uranium").withChance(0.06),
-            Item.of("kubejs:thorium_grit").withChance(0.06),
-        ],
-        "create_things_and_misc:crushed_magma",
-    )
-    event.smelting(
-        "kubejs:thorium_ingot",
-        "kubejs:thorium_grit"
+  event.recipes.create.splashing(
+    [
+      Item.of("mekanism:dirty_dust_uranium").withChance(0.06),
+      Item.of("kubejs:thorium_grit").withChance(0.06),
+    ],
+    "create_things_and_misc:crushed_magma",
+  )
+  event.smelting(
+    "kubejs:thorium_ingot",
+    "kubejs:thorium_grit",
+  )
+  event.custom(
+    {
+      "type": "mekanism:oxidizing",
+      "input":
+      {
+        "ingredient":
+        {
+          "item": "kubejs:protactinium",
+        },
+      },
+        "output":
+        {
+          "amount": 100,
+          "gas": "kubejs:protactinium_oxide",
+        },
+      },
     )
     event.custom(
-    {
-        "type": "mekanism:oxidizing",
-        "input": {
-          "ingredient": {
-            "item": "kubejs:protactinium"
-          }
+      {
+        "type": "mekanism:activating",
+        "input":
+        {
+          "amount": 10,
+          "gas": "kubejs:protactinium_oxide",
         },
-        "output": {
-          "amount": 100,
-          "gas": "kubejs:protactinium_oxide"
-        }
-      })
-    event.custom({        
-      "type": "mekanism:activating",
-      "input": {
-        "amount": 10,
-        "gas": "kubejs:protactinium_oxide"
+        "output":
+          {
+            "amount": 10,
+            "gas": "mekanism:uranium_oxide",
+          },
       },
-      "output": {
-        "amount": 10,
-        "gas": "mekanism:uranium_oxide"
-      },
-   })
+    )
 })
